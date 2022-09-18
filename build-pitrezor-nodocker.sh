a836d0e8e1f5ab412a2d78b6d9f63e22ff827850
3rd Iteration <steve@cryptoguide.tips>
2022-09-18 15:28:17 -0400

    Create build-pitrezor-nodocker.sh

---
+++ build-pitrezor-nodocker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+IMAGE=pitrezor-build
+TAG=${1:-master}
+IMGFILE=images/pitrezor-$TAG.img
+
+mkdir -p images
+git checkout $TAG
+git submodule update --init --recursive
+. poky/oe-init-build-env build
+bitbake pitrezor-image
+cp tmp/deploy/images/raspberrypi0-wifi/pitrezor-image-raspberrypi0-wifi.rpi-sdimg /$IMGFILE
+
+zip -m -j -9 ${IMGFILE%.img}.zip $IMGFILE
+
