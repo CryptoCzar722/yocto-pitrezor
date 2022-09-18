d6663688ca7d5339a420a44357bbd7ed119a516b
3rd Iteration <steve@cryptoguide.tips>
2022-09-18 15:35:35 -0400

    Fix permissions

--- build-pitrezor-nodocker.sh
+++ build-pitrezor-nodocker.sh
@@ -10,6 +10,6 @@ git checkout $TAG
 git submodule update --init --recursive
 . poky/oe-init-build-env build
 bitbake pitrezor-image
-cp tmp/deploy/images/raspberrypi0-wifi/pitrezor-image-raspberrypi0-wifi.rpi-sdimg /$IMGFILE
 
+cp tmp/deploy/images/raspberrypi0-wifi/pitrezor-image-raspberrypi0-wifi.rpi-sdimg /$IMGFILE
 zip -m -j -9 ${IMGFILE%.img}.zip $IMGFILE
